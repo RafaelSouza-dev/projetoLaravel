e7301b34d58d4a8b165dd048574eb9638e7e4c33
RafaelSouza-dev <rafaelsouza.drm@gmail.com>
2022-05-11 09:54:55 -0300

    mudanças na dasboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <?php /*
-    <!--<x-slot name="header">
+
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{__('Dashboard') }}
         </h2>
@@ -12,7 +12,7 @@
                 <x-jet-welcome />
             </div>
         </div>
-    </div> */?>
+    </div>
 
 <div class="containerPrincipal">
     @yield('content2')
